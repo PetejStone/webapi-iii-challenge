8830f5f1690c4c5ac58faa686469fbcbf04e5d9c
PetejStone <petejstoney@gmail.com>
2019-05-29 16:45:00 -0700

    created validatePost

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
 const server = express();
 const Users = require('./users/userDb.js')
+const Posts = require('./posts/postDb.js')
 
 //global middleware
 server.use(express.json());
@@ -22,20 +23,39 @@ if (id) {
 });
 
 function validateUser(req, res, next) {
-  console.log(Object.keys(req.body).length)
-  const bodyLength = Object.keys(req.body);
+  //console.log(Object.keys(req.body).length)
+  const bodyLength = Object.keys(req.body);//converts object to array to get length
   const username = req.body;
   if (req.body && req.body.name) {
     next();
   }
   if (bodyLength.length <= 0)  {
-    res.status(400).json({message: 'no data'})
+    res.status(400).json({message: 'missing user data'})
   }
   if ( !username.name ) {
-    res.status(400).json({message: 'no name'})
+    res.status(400).json({message: 'missing required name field'})
   }
   
 }
+
+function validatePost(req, res, next) {
+  //console.log(Object.keys(req.body).length)
+  const bodyLength = Object.keys(req.body);//converts object to array to get length
+  const username = req.body;
+  if (req.body && req.body.text) {
+    next();
+  }
+  if (bodyLength.length <= 0)  {
+    res.status(400).json({message: 'missing post data'})
+  }
+  if ( !username.text ) {
+    res.status(400).json({message: 'missing required text field'})
+  }
+  
+}
+
+//////END OF MIDDLEWARE
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -65,6 +85,20 @@ server.get('/users/:id', validateUserId, async (req, res) => {
   }
 });
 
+server.get('/users/:id/posts',  async (req, res) => {
+  try {
+    const id = await Users.getUserPosts(req.params.id);
+      res.status(200).json(id);
+    
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the user',
+    });
+  }
+});
+
 server.post('/users', validateUser, async(req, res) => {
   try {
     const user = await Users.insert(req.body);
@@ -76,6 +110,22 @@ server.post('/users', validateUser, async(req, res) => {
       message: 'Error adding the user',
     });
   }
+})
+
+server.post('/users/:id/posts', validatePost, async(req, res) => {
+  //const postInfo = {...req.body, user_id: req.params.id}
+  const messageInfo = { ...req.body, user_id: req.params.id };
+
+    try {
+      const message = await Posts.insert(messageInfo);
+      res.status(210).json(message);
+    } catch (error) {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error getting the messages for the hub',
+      });
+    }
 })
 
 
